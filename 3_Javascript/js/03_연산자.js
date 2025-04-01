3b7aba2064acd6e382bf196dcdb8d30c393ac7a8
dudals3530 <dudals3530@naver.com>
2025-04-02 00:19:10 +0900

    봄버맨 개웃김..ㅋㅋㅋ

--- 3_Javascript/js/03_연산자.js
+++ 3_Javascript/js/03_연산자.js
@@ -10,65 +10,57 @@ const number1 = document.getElementById("input1");
 
 const nmuber2 = document.getElementById("input2");
 
-
 const result = document.getElementById("calcResult");
 
 // 두수를 더해서 화면에 출력하는 함수
-function plusFn(){
-  
+function plusFn() {
   //input요소.value : input 요소에 작성된 값 얻어오기
   const value1 = number1.value;
   const value2 = nmuber2.value;
 
-  console.log(value1,value2);
+  console.log(value1, value2);
   // -> input 요소에 작성된 값은 무조건 문자열 (String) 형태라서
   // 더했을 때 이어쓰기 되는 문제가 발생한다!
 
-  console.log(value1+value2);
+  console.log(value1 + value2);
 
   //[해결방법]
   // 문자열을 숫자로 변경하는 코드를 수행
   // 숫자만 작성된 문자열 ("123") 을
   // 진짜 숫자 타입으로 바꾸는 방법
   // - > Number("123");
-  console.log(Number(value1)+Number(value2));
+  console.log(Number(value1) + Number(value2));
 
   // 두수의 합을
   // 아이디가 "calcResult" 인 요소 (reslut 변수)의
   // 내부 글자(innerText, HTML의 content)로 대입하기
-  result.innerText = Number(value1)+Number(value2);
+  result.innerText = Number(value1) + Number(value2);
 }
 
-function minusFn(){
-   const value1 = input1.value;
-   const value2 = input2.value;
-
-   result.innerText = Number(value1)-Number(value2);
-   
-}
-function multiFn(){
+function minusFn() {
   const value1 = input1.value;
   const value2 = input2.value;
 
-  result.innerText = Number(value1)*Number(value2);
-  
+  result.innerText = Number(value1) - Number(value2);
 }
-function divFn(){
+function multiFn() {
   const value1 = input1.value;
   const value2 = input2.value;
 
-  result.innerText = Number(value1)/Number(value2);
-  
+  result.innerText = Number(value1) * Number(value2);
 }
-function modFn(){
+function divFn() {
   const value1 = input1.value;
   const value2 = input2.value;
 
-  result.innerText = Number(value1)%Number(value2);
-  
+  result.innerText = Number(value1) / Number(value2);
 }
+function modFn() {
+  const value1 = input1.value;
+  const value2 = input2.value;
 
-
+  result.innerText = Number(value1) % Number(value2);
+}
 
 const num1 = document.getElementById("num1");
 const num2 = document.getElementById("num2");
@@ -76,14 +68,10 @@ const num3 = document.getElementById("num3");
 
 const total = document.getElementById("total");
 
-function totalFn(){
+function totalFn() {
+  const value1 = num1.value;
+  const value2 = num2.value;
+  const value3 = num3.value;
 
-  const value1=num1.value;
-  const value2=num2.value;
-  const value3=num3.value;
-
-  total.innerText= Number(value1)+Number(value2)+Number(value3)
+  total.innerText = Number(value1) + Number(value2) + Number(value3);
 }
-
-
-
--- 3_Javascript/js/봄버맨.js
+++ 3_Javascript/js/봄버맨.js
@@ -1,187 +1,220 @@
-
-const bomberman =document.querySelector("#bomberman");
+const bomberman = document.querySelector("#bomberman");
+const goblinCountSpan = document.querySelector("#goblinCount");
 
 let bombs = [];
-
 let position = { x: 0, y: 0 }; // 초기 위치를 저장하는 객체
-
 let goblins = []; // 고블린을 저장하는 배열
 let goblinPosition = { x: 200, y: 200 };
+let destroyedGoblinCount = 0; // 이건 변신조건 count
+let isTransformed = false; // 봄버맨이 변신 상태인지 확인하는 변수
+let goblinsRemovedCount = 0; //고블린현재 몇마리잡앗는지
 
-
-
-document.addEventListener("keydown", function(e){
-  
- /* console.log(`Key: ${e.key}, KeyCode: ${e.keyCode}`);
-                Key: ArrowDown, KeyCode: 40
-    봄버맨.js:5 Key: ArrowUp, KeyCode: 38
-    봄버맨.js:5 Key: ArrowLeft, KeyCode: 37
-    봄버맨.js:5 Key: ArrowRight, KeyCode: 39  */
-
-    switch (e.key) {
-      case "ArrowDown":
-        position.y += 10;
-        break;
-      case "ArrowUp":
-        position.y -= 10;
-        break;
-      case "ArrowRight":
-        position.x += 10;
-        break;
-      case "ArrowLeft":
-        position.x -= 10;
-        break;
-      case "x" :
-        createBomb(position.x,position.y);
-        break; 
-      case "z" :  
-        boom();
-        break;
-        default : alert("잘못 누르셧습니다");
-    }
-      //봄버맨 위치 저장시켜버려
-    bomberman.style.transform = `translate(${position.x}px, ${position.y}px)`;
-
-    checkCollision();
-  });
-
-  function createBomb(x,y) {
-   const bomb = document.createElement("img");
-    bomb.src = "/images/봄버맨/bomb.png";
-    bomb.style.position = "absolute";
-    
-    // bomberman의 현재 위치 가져오기
-    // getBoundingClientRect() 가 해당 포지션의 x,y 값을 가져오는 느낌 ?
-     const rect = bomberman.getBoundingClientRect();
-     bomb.style.left = `${rect.left}px`;
-     bomb.style.top = `${rect.top}px`;
-
-
-     // 해당 도큐먼트 바디에 해당 bomb를 자식요소로 추가시킨다
-    document.body.appendChild(bomb);
-    bombs.push(bomb);
+document.addEventListener("keydown", function (e) {
+  switch (e.key) {
+    case "ArrowDown":
+      position.y += 10;
+      break;
+    case "ArrowUp":
+      position.y -= 10;
+      break;
+    case "ArrowRight":
+      position.x += 10;
+      break;
+    case "ArrowLeft":
+      position.x -= 10;
+      break;
+    case "x":
+      createBomb(position.x, position.y);
+      break;
+    case "z":
+      boom();
+      break;
+    default:
+      alert("잘못 누르셨습니다");
   }
+  // 봄버맨 위치 저장시켜버려
+  bomberman.style.transform = `translate(${position.x}px, ${position.y}px)`;
 
-  function boom() {
-    if (bombs.length > 0) {
-      for (let i = 0; i < bombs.length; i++) {
-        bombs[i].src = "/images/봄버맨/boomm.png"; // 모든 bomb 요소의 src 속성을 변경
-        bombs[i].style.width = "50px"; // 초기 폭탄 크기
-        bombs[i].style.height = "50px"; // 초기 폭탄 크기
-        
-        // 폭탄의 크기를 10px씩 키우기
-        const increaseSizeInterval = setInterval(() => {
-          let currentWidth = parseInt(bombs[i].style.width);
-          let currentHeight = parseInt(bombs[i].style.height);
-          bombs[i].style.width = `${currentWidth + 20}px`;
-          bombs[i].style.height = `${currentHeight + 20}px`;
-        }, 200);
-  
-        // 고블린과 폭탄의 충돌 체크
-        checkBombCollision(bombs[i]);
-  
-        // increaseSizeInterval을 clear할 필요가 있으므로 bombs[i]에 저장
-        bombs[i].increaseSizeInterval = increaseSizeInterval;
-      }
-  
-      // 모든 폭탄을 2초 후에 제거
-      setTimeout(() => {
-        for (let i = 0; i < bombs.length; i++) {
-          clearInterval(bombs[i].increaseSizeInterval);
-          bombs[i].remove();
+  checkCollision();
+});
+
+function createBomb(x, y) {
+  const bomb = document.createElement("img");
+  bomb.src = "/images/봄버맨/bomb.png";
+  bomb.style.position = "absolute";
+
+  const rect = bomberman.getBoundingClientRect();
+  bomb.style.left = `${rect.left}px`;
+  bomb.style.top = `${rect.top}px`;
+
+  document.body.appendChild(bomb);
+  bombs.push(bomb);
+}
+
+function boom() {
+  if (bombs.length > 0) {
+    for (let i = 0; i < bombs.length; i++) {
+      bombs[i].src = "/images/봄버맨/boomm.png";
+      bombs[i].style.width = "50px";
+      bombs[i].style.height = "50px";
+
+      const increaseSizeInterval = setInterval(() => {
+        let currentWidth = parseInt(bombs[i].style.width);
+        let currentHeight = parseInt(bombs[i].style.height);
+
+        if (currentWidth < 80 && currentHeight < 80) {
+          bombs[i].style.width = `${currentWidth + 10}px`;
+          bombs[i].style.height = `${currentHeight + 10}px`;
+        } else {
+          clearInterval(increaseSizeInterval);
         }
-  
-        // 폭탄 배열 초기화
-        bombs = [];
-      }, 2000);
-    } else {
-      alert("폭탄이 없습니다.");
-    }
-  }
 
+        checkBombCollision(bombs[i]);
+      }, 200);
 
-  function createGoblin() {
-    const goblin = document.createElement("img");
-    goblin.src = "/images/봄버맨/31952459-고블린.jpg";
-    goblin.classList.add("goblin");
-
-    const goblinPosition = { x: Math.random() * (box.clientWidth - 50), y: Math.random() * (box.clientHeight - 50) };
-    goblin.style.left = `${goblinPosition.x}px`;
-    goblin.style.top = `${goblinPosition.y}px`;
-
-    box.appendChild(goblin);
-    goblins.push({ element: goblin, position: goblinPosition });
-
-    moveGoblin(goblin, goblinPosition);}
-
-
-  function moveGoblin(goblin, goblinPosition) {
-    const directions = ["up", "down", "left", "right"];
-    const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-
-
-    switch (randomDirection) {
-      case "up":
-        if (goblinPosition.y > 0) goblinPosition.y -= 10;
-        break;
-      case "down":
-        if (goblinPosition.y < box.clientHeight - goblin.clientHeight) goblinPosition.y += 10;
-        break;
-      case "left":
-        if (goblinPosition.x > 0) goblinPosition.x -= 10;
-        break;
-      case "right":
-        if (goblinPosition.x < box.clientWidth - goblin.clientWidth) goblinPosition.x += 10;
-        break;
+      bombs[i].increaseSizeInterval = increaseSizeInterval;
     }
 
-    goblin.style.transform = `translate(${goblinPosition.x}px, ${goblinPosition.y}px)`;
-    checkCollision();
+    setTimeout(() => {
+      for (let i = 0; i < bombs.length; i++) {
+        clearInterval(bombs[i].increaseSizeInterval);
+        bombs[i].remove();
+      }
+
+      bombs = [];
+    }, 2000);
+  } else {
+    alert("폭탄이 없습니다.");
+  }
+}
+
+function createGoblin() {
+  if (goblins.length >= 6) {
+    return;
   }
 
-  // 0.5초마다 고블린을 움직이게 하는 interval 설정
-  setInterval(function() {
-    goblins.forEach(goblinObj => {
-      moveGoblin(goblinObj.element, goblinObj.position);
-    });
-  }, 500);
+  const goblin = document.createElement("img");
+  goblin.src = "/images/봄버맨/31952459-고블린.jpg";
+  goblin.classList.add("goblin");
+  goblin.style.border = "1px solid red";
 
-  // 2초마다 새로운 고블린을 추가하는 interval 설정
-  setInterval(createGoblin, 2000);
+  const goblinPosition = {
+    x: Math.random() * (box.clientWidth - 50),
+    y: Math.random() * (box.clientHeight - 50),
+  };
+  goblin.style.left = `${goblinPosition.x}px`;
+  goblin.style.top = `${goblinPosition.y}px`;
 
-  function checkCollision() {
-    const bombermanRect = bomberman.getBoundingClientRect();
+  box.appendChild(goblin);
+  goblins.push({ element: goblin, position: goblinPosition });
 
-    goblins.forEach(goblinObj => {
-      const goblinRect = goblinObj.element.getBoundingClientRect();
+  moveGoblin(goblin, goblinPosition);
+}
 
-      if (
-        bombermanRect.left < goblinRect.right &&
-        bombermanRect.right > goblinRect.left &&
-        bombermanRect.top < goblinRect.bottom &&
-        bombermanRect.bottom > goblinRect.top
-      ) {
+function moveGoblin(goblin, goblinPosition) {
+  const directions = ["up", "down", "left", "right"];
+  const randomDirection =
+    directions[Math.floor(Math.random() * directions.length)];
+
+  switch (randomDirection) {
+    case "up":
+      if (goblinPosition.y > 0) goblinPosition.y -= 10;
+      break;
+    case "down":
+      if (goblinPosition.y < box.clientHeight - goblin.clientHeight)
+        goblinPosition.y += 10;
+      break;
+    case "left":
+      if (goblinPosition.x > 0) goblinPosition.x -= 10;
+      break;
+    case "right":
+      if (goblinPosition.x < box.clientWidth - goblin.clientWidth)
+        goblinPosition.x += 10;
+      break;
+  }
+
+  goblin.style.transform = `translate(${goblinPosition.x}px, ${goblinPosition.y}px)`;
+  checkCollision();
+}
+
+// 0.5초마다 고블린을 움직이게 하는 interval 설정
+setInterval(function () {
+  goblins.forEach((goblinObj) => {
+    moveGoblin(goblinObj.element, goblinObj.position);
+  });
+}, 500);
+
+// 2초마다 새로운 고블린을 추가하는 interval 설정
+setInterval(createGoblin, 2000);
+
+function checkCollision() {
+  const bombermanRect = bomberman.getBoundingClientRect();
+
+  goblins.forEach((goblinObj) => {
+    const goblinRect = goblinObj.element.getBoundingClientRect();
+
+    if (
+      bombermanRect.left < goblinRect.right &&
+      bombermanRect.right > goblinRect.left &&
+      bombermanRect.top < goblinRect.bottom &&
+      bombermanRect.bottom > goblinRect.top
+    ) {
+      if (isTransformed) {
+        console.log("충돌: 고블린 제거");
+        goblinObj.element.remove();
+        goblins.splice(goblins.indexOf(goblinObj), 1);
+        goblinsRemovedCount++;
+        goblinCountSpan.textContent = goblinsRemovedCount;
+      } else {
+        console.log("충돌: 봄버맨 사망");
         alert("봄버맨이 죽었어요 ㅠ^ㅠ");
         document.documentElement.innerHTML = document.documentElement.innerHTML; // 페이지 리셋
       }
-    });
+    }
+  });
+}
+
+function checkBombCollision(bomb) {
+  const bombRect = bomb.getBoundingClientRect();
+
+  goblins.forEach((goblinObj, index) => {
+    const goblinRect = goblinObj.element.getBoundingClientRect();
+
+    if (
+      bombRect.left < goblinRect.right &&
+      bombRect.right > goblinRect.left &&
+      bombRect.top < goblinRect.bottom &&
+      bombRect.bottom > goblinRect.top
+    ) {
+      // 고블린을 제거
+      goblinObj.element.remove();
+      goblins.splice(index, 1);
+      destroyedGoblinCount++;
+      goblinsRemovedCount++;
+      goblinCountSpan.textContent = goblinsRemovedCount;
+      checkGoblinDestructionCount();
+    }
+  });
+}
+
+function checkGoblinDestructionCount() {
+  if (destroyedGoblinCount >= 3) {
+    transformBomberman();
+    destroyedGoblinCount = 0; // 초기화
   }
+}
+function transformBomberman() {
+  isTransformed = true;
+  bomberman.src = "/images/봄버맨/초사이어인.png"; // 변신 상태를 시각적으로 표시 (예: 이미지 변경)
+  bomberman.style.width = "100px"; // 변신 시 사이즈 키우기
+  bomberman.style.height = "100px"; // 변신 시 사이즈 키우기
+  console.log("봄버맨 변신");
 
-  function checkBombCollision(bomb) {
-    const bombRect = bomb.getBoundingClientRect();
-
-    goblins.forEach((goblinObj, index) => {
-      const goblinRect = goblinObj.element.getBoundingClientRect();
-
-      if (
-        bombRect.left < goblinRect.right &&
-        bombRect.right > goblinRect.left &&
-        bombRect.top < goblinRect.bottom &&
-        bombRect.bottom > goblinRect.top
-      ) {
-        // 고블린을 제거
-        goblinObj.element.remove();
-        goblins.splice(index, 1);
-      }
-    });
-  }
+  setTimeout(() => {
+    isTransformed = false;
+    bomberman.src = "/images/봄버맨/bomberman.png"; // 원래 상태로 복귀 (예: 원래 이미지로 복귀)
+    bomberman.style.width = "50px"; // 원래 사이즈로 복귀
+    bomberman.style.height = "50px"; // 원래 사이즈로 복귀
+    console.log("봄버맨 원래 상태로 복귀");
+  }, 5000); // 변신 상태 5초 유지
+}
